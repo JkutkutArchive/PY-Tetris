2a928bfe5b0ced73fad733645b6cdae6f9b40dcb
Jkutkut <jorgeregonzalez@gmail.com>
2020-05-11 15:36:42 +0200

    start on classes + color.random

--- tetris.py
+++ tetris.py
@@ -14,38 +14,25 @@ def constant(f): #define of a constant class
         return f()
     return property(fget, fset)
 
-class color(object): #Create the COLOR class as a collection of constants
-    @constant
-    def BG(): # background
-        # return (25, 25, 25) 
-        return (255, 255, 255) 
-    @constant
-    def GRID(): # Color of the grid
-        return (128, 128, 128) 
-    @constant
-    def BLACK(): # Color of the grid
-        return (0, 0, 0)
-    @constant
-    def WHITE():
-        return (255, 255, 255)
-    @constant
-    def BLUE():
-        return (0, 153, 255)
-    @constant
-    def PINK():
-        return (255, 0, 255)
-    @constant
-    def GREEN():
-        return (102, 255, 102)
-    @constant
-    def YELLOW():
-        return (255, 255, 102)
-    @constant
-    def ORANGE():
-        return (255, 102, 0)
-    @constant
-    def RED():
-        return (255, 80, 80)
+class color():
+    def __init__(self):
+        self.BG = (255, 255, 255)
+        self.GRID = (128, 128, 128)
+        self.BLACK = (0, 0, 0)
+        self.WHITE = (255, 255, 255)
+        self.DBLUE = (0, 153, 255)
+        self.LBLUE = (102, 255, 255)
+        self.PINK = (255, 0, 255)
+        self.GREEN = (102, 255, 102)
+        self.YELLOW = (255, 255, 102)
+        self.ORANGE = (255, 102, 0)
+        self.RED = (255, 80, 80)
+    def RANDOM(self):
+        colorSet = dict(self.__dict__)
+        for x in ["BG", "GRID", "WHITE", "BLACK"]: del colorSet[x] # Remove not wanted colors
+        list = [colorSet[colorTitle] for colorTitle in [colorTitle for colorTitle in colorSet]]
+        return list[np.random.randint(len(list))]
+
 
 def getCubeCoord(x, y, *smaller): # Returns the same coordinates with the margin frame
     smallerC = (sizeWidthX / 15) if smaller else 0 
@@ -57,6 +44,37 @@ def getCubeCoord(x, y, *smaller): # Returns the same coordinates with the margin
         (x * sizeWidthX + smallerC + center, (y + 1) * sizeWidthY - smallerC)
     ]
     return [tuple(map(lambda i: i + marginFrame, tu)) for tu in rawCoord]
+
+class tetrisPiece:
+    def __init__(self, type):
+        self.color = np.random.randint(5)
+        self.type = type
+        self.pieces = []
+        # Straight: |   Square:   |   T:        |   L:        |   Skew:
+        # - - - -   |   - - - -   |   - - - -   |   - 0 - -   |   - - - -
+        # 0 0 0 0   |   - 0 0 -   |   - 0 0 0   |   - 0 - -   |   - - 0 0
+        # - - - -   |   - 0 0 -   |   - - 0 -   |   - 0 0 -   |   - 0 0 -
+
+        # if self.typeConv[self.type] == "Straight":
+        #     for i in range(4):
+        #         self.pieces = self.pieces + [()]
+        # elif self.typeConv[self.type] == "Square":
+        # elif self.typeConv[self.type] == "T":
+        # elif self.typeConv[self.type] == "L":
+        # elif self.typeConv[self.type] == "Skew":
+
+    @constant
+    def typeConv(self): # to convert a int to the equivalent piece
+        return ["Straight", "Square", "T", "L", "Skew"]
+
+class bloq:
+    def __init__(self, x, y, c):
+        self.x = x
+        self.y = y
+        self.color = c
+
+# -------------     CODE      -------------
+
 
 pygame.init() # Init pygame
 pygame.display.set_caption("Jkutkut's Tetris") # Set the title of the game
@@ -83,8 +101,8 @@ levelLabel = font.render('Level:', False, COLOR.BLACK)
 # State of the cells: None = empty, else = filled
 grid = np.matrix([[None for j in range(sizeY)] for i in range(sizeX)])
 
-for i in range(sizeX):
-    grid[i, 5] = COLOR.RED
+for i in range(sizeX-1):
+    grid[i, 5] = COLOR.RANDOM()
 
 gameRunning = True # If false, the game stops
 # timeRunning = False # If true, time runs (so iterations occur)
@@ -95,9 +113,8 @@ while gameRunning:
     for x in range(sizeX): # for each spot in the grid
         for y in range(sizeY):
             # Draw the grid
-            if(grid[x, y] == None):
-                pygame.draw.polygon(screen, COLOR.GRID, getCubeCoord(x, y), 1)
-            else:
+            pygame.draw.polygon(screen, COLOR.GRID, getCubeCoord(x, y), 1)
+            if(grid[x, y] != None):
                 pygame.draw.polygon(screen, grid[x, y], getCubeCoord(x, y, True), 0)
     
     for x in range(4):
