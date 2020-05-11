c5c4e1d409285083bd073b8d4b7ff80ee5883c52
Jkutkut <jorgeregonzalez@gmail.com>
2020-05-11 22:53:18 +0200

    simplified rotation syntaxc

--- tetris.py
+++ tetris.py
@@ -105,55 +105,40 @@ class tetrisPiece:
         return True
 
     def rotate(self):
+        ini = [self.pieces[2].x, self.pieces[2].y]
+        if self.pieces[0].y == self.pieces[1].y: # Horizontal position
+            dh = 1
+            dv = 0
+        else:
+            dh = 0
+            dv = 1
         if self.type == 0: # "Straight"
-            if self.pieces[0].y == self.pieces[1].y: # Horizontal position
-                print("hori")
-                ini = [self.pieces[2].x, self.pieces[2].y]
-                for i in range(-2, 2, 1):
-                    print(i)
-                    self.pieces[i + 2].x = ini[0]
-                    self.pieces[i + 2].y = ini[1] + i
-            else:
-                print("vertical")
-                ini = [self.pieces[2].x, self.pieces[2].y]
-                for i in range(-2, 2, 1):
-                    self.pieces[i + 2].x = ini[0] + i
-                    self.pieces[i + 2].y = ini[1]
+            for i in range(-2, 2, 1):
+                self.pieces[i + 2].x = ini[0] + i * dv
+                self.pieces[i + 2].y = ini[1] + i * dh
+        
+        
+        
+        elif self.type == 5: # "Skew":
+            for i in range(2):
+                self.pieces[i].x = ini[0] - 1 + i * dv
+                self.pieces[i].y = ini[1] + (- 1 + i) * dh + dv
+            self.pieces[3].x = ini[0] + dv
+            self.pieces[3].y = ini[1] + dh
+
+
+
+
+
+        elif self.type == 6: # "Skew'":
+            for i in range(2):
+                self.pieces[i].x = ini[0] + dh + (-1 + i) * dv
+                self.pieces[i].y = ini[1] - 1 + i * dh 
+            self.pieces[3].x = ini[0] + dv
+            self.pieces[3].y = ini[1] + dh
         # elif self.type == 2: # "T"
         # elif self.type == 3: # "L"
         # elif self.type == 4: # "L'"
-        elif self.type == 5: # "Skew":
-            if self.pieces[0].y == self.pieces[1].y: # Horizontal position
-                ini = [self.pieces[2].x, self.pieces[2].y]
-                for i in range(2):
-                    self.pieces[i].x = ini[0] - 1
-                    self.pieces[i].y = ini[1] - 1 + i
-                self.pieces[3].x = ini[0]
-                self.pieces[3].y = ini[1] + 1
-            else:
-                ini = [self.pieces[2].x, self.pieces[2].y]
-                for i in range(2):
-                    self.pieces[i].x = ini[0] - 1 + i
-                    self.pieces[i].y = ini[1] + 1
-                self.pieces[3].x = ini[0] + 1
-                self.pieces[3].y = ini[1]
-
-        elif self.type == 6: # "Skew'":
-            if self.pieces[0].y == self.pieces[1].y: # Horizontal position
-                ini = [self.pieces[2].x, self.pieces[2].y]
-                for i in range(2):
-                    self.pieces[i].x = ini[0] + 1
-                    self.pieces[i].y = ini[1] - 1 + i
-                self.pieces[3].x = ini[0]
-                self.pieces[3].y = ini[1] + 1
-            else:
-                ini = [self.pieces[2].x, self.pieces[2].y]
-                for i in range(2):
-                    self.pieces[i].x = ini[0] - 1 + i
-                    self.pieces[i].y = ini[1] - 1
-                self.pieces[3].x = ini[0] + 1
-                self.pieces[3].y = ini[1]
-
 
     def typeConv(self): # to convert a int to the equivalent piece
         return ["Straight", "Square", "T", "L", "L'", "Skew", "Skew'"]
