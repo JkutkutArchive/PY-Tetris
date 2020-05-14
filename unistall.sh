dbe346daa97275d93e91ab8c86a72ade1edc0a3d
Jkutkut <jorgeregonzalez@gmail.com>
2020-05-15 00:28:24 +0200

    renamed

--- unistall.sh
+++
@@ -1,8 +0,0 @@
-sudo rm /usr/bin/tetris &&
-sudo rm /usr/share/applications/tetris.desktop &&
-sudo rm /usr/share/icons/tetris.png &&
-echo "tetris removed successfully" ||
-echo "
-~~~~~~~~    ERROR AT UNINSTALLATION   ~~~~~~~~
-    Not posible to complete the unistallation
-"
