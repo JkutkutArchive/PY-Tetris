21c7c37ca41627631110b515abda3ca6149108ff
Jkutkut <jorgeregonzalez@gmail.com>
2020-05-15 00:30:19 +0200

    renamed

---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+sudo rm /usr/bin/tetris &&
+sudo rm /usr/share/applications/tetris.desktop &&
+sudo rm /usr/share/icons/tetris.png &&
+echo "tetris removed successfully" ||
+echo "
+~~~~~~~~    ERROR AT UNINSTALLATION   ~~~~~~~~
+    Not posible to complete the unistallation
+"
